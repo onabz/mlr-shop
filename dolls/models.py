2113330d2ecb1fed86a5b8ecfed52f3bcf5fd321
onabz <ONABOLUMBE@GMAIL.COM>
2023-01-16 16:19:49 +0000

    Create dolltype model for dolls app

--- dolls/models.py
+++ dolls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class DollType(models.Model):
+    """ A model to manage doll categories """
+
+    name = models.CharField(max_length=25, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
