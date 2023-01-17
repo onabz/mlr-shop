193c129ada98825a2608ccaa430d9b7475feb7d5
onabz <ONABOLUMBE@GMAIL.COM>
2023-01-17 21:38:17 +0000

    Add blank line

--- dolls/admin.py
+++ dolls/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from .models import Doll, DollType
 from django_summernote.admin import SummernoteModelAdmin
 
+
 @admin.register(DollType)
 
 
 @admin.register(Doll)
 class DollAdmin(SummernoteModelAdmin):
 
+    # list_display = ('name', 'category', 'price', 'image', 'on_sale')
     summernote_fields = ('description', 'safety_note', 'care_love_instructions')
