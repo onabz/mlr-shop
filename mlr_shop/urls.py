178de7d6a085920c950b4213ac25d0b9c25b8ce8
onabz <ONABOLUMBE@GMAIL.COM>
2023-01-20 14:15:17 +0000

    Add path for dolls in project url.py file

--- mlr_shop/urls.py
+++ mlr_shop/urls.py
@@ -20,8 +20,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('summernote/', include('django_summernote.urls')),
+    path('dolls/', include('dolls.urls')),
+    
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
