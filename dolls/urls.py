90279e4018fa68068660cb2f07758f72622f5f36
onabz <ONABOLUMBE@GMAIL.COM>
2023-01-20 19:01:54 +0000

    Add doll detail url path

--- dolls/urls.py
+++ dolls/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_dolls, name='dolls')
+    path('', views.all_dolls, name='dolls'),
+    path('<doll_id>', views.doll_detail, name='doll_detail'),
 ]
