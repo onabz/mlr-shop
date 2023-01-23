eebeb84bda7cc8a5f3af506a1fb2b2616b8ae7ee
onabz <ONABOLUMBE@GMAIL.COM>
2023-01-23 19:46:18 +0000

    Add if statement for dolltype query

--- dolls/views.py
+++ dolls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Doll
+from .models import Doll, DollType
 
 
 def all_dolls(request):
@@ -9,8 +9,14 @@ def all_dolls(request):
 
     dolls = Doll.objects.all()
     query = None
+    dolltype = None
 
     if request.GET:
+        if 'dolltype' in request.GET:
+            dolltypes = request.GET['dolltype']
+            dolls = dolls.filter(dolltype__name__in=dolltypes)
+            dolltypes = DollType.objects.filter(name__in=dolltypes)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -23,6 +29,7 @@ def all_dolls(request):
     context = {
         'dolls': dolls,
         'search_term': query,
+        'current_dolltypes': dolltype,
     }
 
     return render(request, 'dolls/dolls.html', context)
