7b93c3b1e4b3c9ac2c24807224cfc3390b02d0bb
onabz <ONABOLUMBE@GMAIL.COM>
2023-01-24 23:08:09 +0000

    Create add to bag function

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 def view_bag(request):
     """ A view to render shopping bag content page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add product to shopping bag """
+
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
