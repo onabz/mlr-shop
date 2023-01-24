c69c23a8b001358cd0a3b03c5393ee6a4389e03d
onabz <ONABOLUMBE@GMAIL.COM>
2023-01-24 20:38:03 +0000

    Add view for bag template

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def view_bag(request):
+    """ A view to render shopping bag content page """
+
+    return render(request, 'bag/bag.html')
