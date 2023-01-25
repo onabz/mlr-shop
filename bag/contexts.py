a62af1dec6255d3410a50c2889164c8f90585b94
onabz <ONABOLUMBE@GMAIL.COM>
2023-01-25 18:05:39 +0000

    Add logic for shopping bag content

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,16 +1,30 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from dolls.models import Doll
 
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
-    product_count = 0
+    # doll_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id in bag.items():
+        doll = get_object_or_404(Doll, pk=item_id)
+        total += doll.price
+        bag_items.append({
+            'item_id': item_id,
+            'doll': doll,
+        })
+
+    grand_total = total
 
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count,
+        'grand_total': grand_total,
+        # 'doll_count': doll_count,
     }
 
     return context
