a2f58f9280be5eea87da2b53dfa9b217d99a9a24
onabz <ONABOLUMBE@GMAIL.COM>
2023-01-25 18:07:34 +0000

    Add code for adding items to bag

--- bag/views.py
+++ bag/views.py
@@ -13,6 +13,8 @@ def add_to_bag(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
+    for item_id in list(bag.keys()):
+        bag[item_id] = item_id
+
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
