5dc32386b7f4f12f821b9511ea03e9bf01af5de6
onabz <ONABOLUMBE@GMAIL.COM>
2023-01-29 18:18:54 +0000

    Add doll count

--- bag/contexts.py
+++ bag/contexts.py
@@ -7,7 +7,7 @@ def bag_contents(request):
 
     bag_items = []
     total = 0
-    # doll_count = 0
+    doll_count = 0
     bag = request.session.get('bag', {})
 
     for item_id in bag.items():
@@ -24,7 +24,7 @@ def bag_contents(request):
         'bag_items': bag_items,
         'total': total,
         'grand_total': grand_total,
-        # 'doll_count': doll_count,
+        'doll_count': doll_count,
     }
 
     return context
