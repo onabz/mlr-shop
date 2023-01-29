24ec6a0cae16fe802385a12daadb7d81618eb29c
onabz <ONABOLUMBE@GMAIL.COM>
2023-01-29 17:21:28 +0000

    Add more stripe elements to views.py

--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,41 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+from bag.contexts import bag_contents
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There is nothing in your bag")
         return redirect(reverse('dolls'))
 
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51MVbQwJDbOhf0bAhrpA38YaUHVOI2z9wRP2kRCvwRToBkaDTVwc2rJCF6pGX8VpxaTj1WmqzTPCmElxWaJ0SYIzI00bz191WYU',
-        'client_secret': 'sk_test_51MVbQwJDbOhf0bAhWMuIR6c7NnBQlui8kyk5fuIMb51A0ENpS2KogXVBfqNMnbCRX0W9zDvVsooYDkNdxhHq0mkF00L3q27myj',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
