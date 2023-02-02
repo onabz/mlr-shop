d28ea696516a8e2ead57ce342427bd2c0489b1f1
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-02 11:31:17 +0000

    Create view for adjust_bag function

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from dolls.models import Doll
@@ -23,13 +23,28 @@ def add_to_bag(request, item_id):
     else:
         bag[item_id] = quantity
 
-    # for item_id in list(bag.keys()):
-    #     bag[item_id]
     #     messages.success(request, f'Added {doll.name} to your bag')
 
     request.session['bag'] = bag
-    # print(request.session['bag'])
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust the quantity of the specified product to the specified amount """
+
+    doll = get_object_or_404(Doll, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    #     messages.success(request, f'Added {doll.name} to your bag')
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
 
 
 def remove_from_bag(request, item_id):
@@ -38,7 +53,7 @@ def remove_from_bag(request, item_id):
     try:
         doll = get_object_or_404(Doll, pk=item_id)
         bag.pop(item_id)
-        messages.success(request, f'Removed {doll.name} from your bag')
+        # messages.success(request, f'Removed {doll.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
