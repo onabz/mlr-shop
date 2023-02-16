82f141187b69b9050f11da14976f4c28d28e2c00
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-16 11:20:43 +0000

    Add user profile field to order admin page

--- checkout/admin.py
+++ checkout/admin.py
@@ -10,9 +10,9 @@ class OrderLineItemAdminInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
-    readonly_fields = ('order_number', 'user_profile', 'date', 'order_total', 'grand_total', 'original_bag', 'stripe_pid')
+    readonly_fields = ('order_number', 'date', 'order_total', 'grand_total', 'original_bag', 'stripe_pid')
 
-    fields = ('order_number', 'date', 'full_name', 'email', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address1', 'street_address2', 'county', 'order_total', 'grand_total', 'original_bag', 'stripe_pid')
+    fields = ('order_number', 'user_profile', 'date', 'full_name', 'email', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address1', 'street_address2', 'county', 'order_total', 'grand_total', 'original_bag', 'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'grand_total',)
