8e578d39d29ce6a40d971aca0641f0899a22f681
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-18 07:13:56 +0000

    Create form for adding dolltypes

---
+++ dolls/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Doll, DollType
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Doll
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        dolltypes = DollType.objects.all()
+
+        names = [(d.id, d.__str__()) for d in dolltypes]
+
+        self.fields['dolltype'].choices = names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
