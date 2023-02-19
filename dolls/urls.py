833f139f6b5c5ea0f76ac7d26deccf8c05e6c2dc
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-19 16:23:07 +0000

    Add url for edit doll

--- dolls/urls.py
+++ dolls/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_dolls, name='dolls'),
     path('<int:doll_id>', views.doll_detail, name='doll_detail'),
     path('add/', views.add_doll, name='add_doll'),
+    path('edit/<int:doll_id>/', views.edit_doll, name='edit_doll'),
 ]
