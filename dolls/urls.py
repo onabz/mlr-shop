4cc64122a1ef0e93475958eab717513210aa97c7
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-19 16:56:59 +0000

    Add url for delete doll

--- dolls/urls.py
+++ dolls/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:doll_id>', views.doll_detail, name='doll_detail'),
     path('add/', views.add_doll, name='add_doll'),
     path('edit/<int:doll_id>/', views.edit_doll, name='edit_doll'),
+    path('delete/<int:doll_id>/', views.delete_doll, name='delete_doll'),
 ]
