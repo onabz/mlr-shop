79487b657477329c8ca2ecce7a648ab7ef1e1259
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-21 12:07:36 +0000

    Add static files storage setting to settings file

--- mlr_shop/settings.py
+++ mlr_shop/settings.py
@@ -170,6 +170,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'  # noqa
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
