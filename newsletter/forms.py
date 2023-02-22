9a065a03f9eb944a0871671eac0c1aa171f009e2
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-22 22:03:50 +0000

    Create send message form

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Newsletter
+from .models import Newsletter, SendMessage
 
 
 class NewsletterForm(forms.ModelForm):
     class Meta:
         model = Newsletter
         fields = ['email',]
+
+
+class SendMessageForm(forms.ModelForm):
+    class Meta:
+        model = SendMessage
+        fields = '__all__'
