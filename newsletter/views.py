c0cbf06aa2c879931ff4e7776368f43b1b7eef14
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-22 20:46:27 +0000

    Create view for newsletter

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Newsletter
+from .forms import NewsletterForm
+from django.contrib import messages
 
-# Create your views here.
+
+def newsletter(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Subscription Successful')
+            return redirect('/')
+    else:
+        form = NewsletterForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'newsletter/newsletter.html', context)
