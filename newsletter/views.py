3413f4390fde45d7e2b47fcc24de5e21712117a3
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-22 22:05:50 +0000

    Create view for send message

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Newsletter
-from .forms import NewsletterForm
+from .models import Newsletter, SendMessage
+from .forms import NewsletterForm, SendMessageForm
 from django.contrib import messages
 
 
@@ -17,3 +17,11 @@ def newsletter(request):
         'form': form,
     }
     return render(request, 'newsletter/newsletter.html', context)
+
+
+def send_message(request):
+    form = SendMessageForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'newsletter/send_message.html', context)
