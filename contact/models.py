2ec27ad58e5ca7a706d14aefb30a828ff56e2a5f
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-23 14:31:36 +0000

    Create contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    email = models.EmailField(null=False, blank=False)
+    message = models.TextField(null=False)
+
+    def __str__(self):
+        return self.name
