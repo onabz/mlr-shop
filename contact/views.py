64ec3a9034212003171d49ca69992046d648cb68
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-23 14:32:24 +0000

    Add logic to view for contact page

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
+from .forms import ContactForm
+from django.contrib import messages
 
 
 def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Details Sent Successfully')
+            return redirect('/')
+    else:
+        form = ContactForm()
     context = {
-
+        'form': form,
     }
     return render(request, 'contact/contact.html', context)
