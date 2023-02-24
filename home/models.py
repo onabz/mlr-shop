6c0633cc2e1d5636daac469292eb6fd2f48108bd
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-24 21:10:36 +0000

    Add custom model for random message on home page

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
+from django.db import models
 
 
-# Create your models here.
+class Saying(models.Model):
+    """ A model to manage random sayings on the home page """
+
+    message = models.CharField(max_length=150, null=False, blank=False)
+
+    def __str__(self):
+        return self.message
