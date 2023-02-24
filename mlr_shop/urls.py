e27e747b10fd546ce48dc67d24696d53009875db
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-24 18:58:51 +0000

    Remove summernote from urls

--- mlr_shop/urls.py
+++ mlr_shop/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('summernote/', include('django_summernote.urls')),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('dolls/', include('dolls.urls')),
