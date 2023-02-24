d4c5f40acc5c820914bb1fc9e347018ddb40b063
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-24 18:58:36 +0000

    Remove summernote from forms

--- dolls/forms.py
+++ dolls/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django_summernote.widgets import SummernoteWidget
 from .models import Doll, DollType
 
 
@@ -8,11 +7,6 @@ class DollForm(forms.ModelForm):
     class Meta:
         model = Doll
         fields = '__all__'
-        widgets = {
-            'description': SummernoteWidget(),
-            'safety_note': SummernoteWidget(),
-            'care_love_instructions': SummernoteWidget(),
-        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
