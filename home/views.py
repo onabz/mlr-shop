2a8eba1621eb1f2b4d6b38d9aed453a3c0799fc6
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-24 21:11:21 +0000

    Pick a random saying from models on home page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
+import random
 from django.shortcuts import render
+from .models import Saying
 
 
 def index(request):
     """ A view to return the index page """
-
-    return render(request, 'home/index.html')
+    sayings = list(Saying.objects.all())
+    random_message = random.choice(sayings)
+    template = 'home/index.html'
+    context = {
+        'message': random_message
+    }
+    return render(request, template, context)
