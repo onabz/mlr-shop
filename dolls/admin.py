349e6f348c274bec4722d9c5002507f0191d75d6
onabz <ONABOLUMBE@GMAIL.COM>
2023-02-24 18:58:25 +0000

    Remove summernote from admin dashboard

--- dolls/admin.py
+++ dolls/admin.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 from .models import Doll, DollType
-from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Doll)
-class DollAdmin(SummernoteModelAdmin):
-
+class DollAdmin(admin.ModelAdmin):
     list_display = ('name', 'dolltype', 'price', 'image', 'on_sale')
     list_filter = ('dolltype',)
-    summernote_fields = (
-        'description', 'safety_note', 'care_love_instructions'
-    )
 
 
 @admin.register(DollType)
